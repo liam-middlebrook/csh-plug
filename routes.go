eada00e52b744ac205756dbd25eba49c19bcb3ee
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-08-02 23:37:07 -0700

    log plug pre-signing

--- routes.go
+++ routes.go
@@ -23,5 +23,17 @@ func index(c *gin.Context){
 func action(c *gin.Context){
     plug := GetPlug()
     url := S3PresignPlug(plug)
+
+    claims, ok := c.Value(csh_auth.AuthKey).(csh_auth.CSHClaims)
+    if !ok {
+        log.Fatal("error finding claims")
+        return
+    }
+    log.WithFields(log.Fields{
+        "uid": claims.UserInfo.Username,
+        "plug_id": plug.ID,
+        "plug_s3id": plug.S3ID,
+        "presigned_uri": url.String(),
+    }).Info("Presigned URI Generated")
     c.Redirect(http.StatusFound, url.String())
 }
