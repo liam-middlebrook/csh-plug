a8530523515e612095d846ce7291f2f5bfe15ca1
Spencer K <sometimestanley@gmail.com>
2018-02-07 02:13:27 -0500

    Forgot to uncomment the modal, whoops.

--- routes.go
+++ routes.go
@@ -284,7 +284,7 @@ func upload_view(c *gin.Context) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
     <script>
-        <!--        $('#agreementModal').modal('show') -->
+        $('#agreementModal').modal('show')
     </script>
 
 </body>
